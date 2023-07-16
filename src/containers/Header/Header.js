e4b8990609ea85d8ec70b09a07baac816534db13
Amie <0950080034@sv.hcmunre.edu.vn>
2023-07-16 21:53:35 +0700

    update responesive

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,80 +1,95 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
 import * as actions from "../../store/actions";
-import Navigator from '../../components/Navigator';
-import { adminMenu , doctorMenu} from './menuApp';
-import './Header.scss';
-import _ from 'lodash';
-import {LANGUAGES,USER_ROLE} from "../../utils"
+import Navigator from "../../components/Navigator";
+import { adminMenu, doctorMenu } from "./menuApp";
+import "./Header.scss";
+import _ from "lodash";
+import { LANGUAGES, USER_ROLE } from "../../utils";
 class Header extends Component {
-    handelChangeLanguge = (language)=>{
-        this.props.changeLanguageAppRedux(language)
+  handelChangeLanguge = (language) => {
+    this.props.changeLanguageAppRedux(language);
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuApp: [],
+    };
+  }
+  componentDidMount() {
+    let { userInfo } = this.props;
+    let menu = [];
+    if (userInfo && !_.isEmpty(userInfo)) {
+      let role = userInfo.roleId;
+      if (role === USER_ROLE.ADMIN) {
+        menu = adminMenu;
+      } else if (role === USER_ROLE.DOCTOR) {
+        menu = doctorMenu;
+      }
     }
-    constructor(props){
-        super(props);
-        this.state = {
-            menuApp:[]
-        }
-    }
-    componentDidMount(){
-        let {userInfo} = this.props;
-        let menu= []
-        if(userInfo && !_.isEmpty(userInfo)){
-            let role= userInfo.roleId;
-            if(role === USER_ROLE.ADMIN){
-                menu = adminMenu
-            } else if  (role === USER_ROLE.DOCTOR){
-                menu = doctorMenu
-            }
-        }
-        this.setState({menuApp:menu})
-        console.log('checking user info: ',this.props.userInfo);
-    }
-    render() {
-        const { processLogout,language,userInfo } = this.props;
-        return (
-            <div className="header-container">
-                {/* thanh navigator */}
-                <div className="header-tabs-container">
-                    <Navigator menus={this.state.menuApp} />
-                </div>
-                <div className='languages'>
-                    <span className='welcome'><FormattedMessage id="home-header.welcome"/>
-                     {userInfo && userInfo.firstName ? userInfo.firstName:''} !
-                    </span>
-                    <span className= {language === LANGUAGES.VI ? "language-vi active" : "language-vi"}
-                        onClick={()=> this.handelChangeLanguge(LANGUAGES.VI)}>
-                            VN
-                        </span>
-                    <span className={language === LANGUAGES.EN ? "language-en active" : "language-en"} 
-                        onClick={()=> this.handelChangeLanguge(LANGUAGES.EN)}>
-                            EN
-                    </span>
-                    {/* nút logout */}
-                    <div className="btn btn-logout" onClick={processLogout} title='Log out'>
-                        <i className="fas fa-sign-out-alt"></i>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
+    this.setState({ menuApp: menu });
+    console.log("checking user info: ", this.props.userInfo);
+  }
+  render() {
+    const { processLogout, language, userInfo } = this.props;
+    return (
+      <div className="header-container">
+        {/* thanh navigator */}
+        <div className="header-tabs-container">
+          <Navigator menus={this.state.menuApp} />
+        </div>
+        <div className="languages">
+          <span className="welcome">
+            <FormattedMessage id="home-header.welcome" />
+            {userInfo && userInfo.firstName ? userInfo.firstName : ""}!
+          </span>
+          <span className="two-languages">
+            <span
+              className={
+                language === LANGUAGES.VI ? "language-vi active" : "language-vi"
+              }
+              onClick={() => this.handelChangeLanguge(LANGUAGES.VI)}
+            >
+              VN
+            </span>
+            <span
+              className={
+                language === LANGUAGES.EN ? "language-en active" : "language-en"
+              }
+              onClick={() => this.handelChangeLanguge(LANGUAGES.EN)}
+            >
+              EN
+            </span>
+          </span>
+          {/* nút logout */}
+          <span
+            className="btn btn-logout"
+            onClick={processLogout}
+            title="Log out"
+          >
+            <i className="fas fa-sign-out-alt"></i>
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn,
-        userInfo: state.user.userInfo,
-        language: state.app.language,
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    userInfo: state.user.userInfo,
+    language: state.app.language,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        processLogout: () => dispatch(actions.processLogout()),
-        changeLanguageAppRedux:(language)=> dispatch(actions.changeLanguageApp(language)),
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    processLogout: () => dispatch(actions.processLogout()),
+    changeLanguageAppRedux: (language) =>
+      dispatch(actions.changeLanguageApp(language)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/containers/HomePage/HomeFooter.js
+++ src/containers/HomePage/HomeFooter.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
 class HomeFooter extends Component {
-    render() {
-        return (
-            <div className='home-footer'>
-                <p>&copy; <FormattedMessage id="home-footer.source" /> </p>
-                <p><FormattedMessage id="home-footer.contact" /> 
-                    <a target="_blank" href='https://www.facebook.com/'>
-                         &#8594; 
-                         <span style={{color: `blue`,}}>
-                            <FormattedMessage id="home-footer.here" />  
-                         </span>
-                         &#8592;
-                    </a> 
-                </p>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="home-footer">
+        <p>
+          &copy; <FormattedMessage id="home-footer.source" />{" "}
+        </p>
+        <p>
+          <FormattedMessage id="home-footer.contact" />
+          <a target="_blank" href="https://www.facebook.com/">
+            <span style={{ color: `blue` }}>
+              &#8594;
+              <FormattedMessage id="home-footer.here" />
+              &#8592;
+            </span>
+          </a>
+        </p>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeFooter);
